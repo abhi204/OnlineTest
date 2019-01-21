38252acfead3a66b4d36a4017fe0c99fcf11b888
Abhi <abhi.chaurasia204@gmail.com>
2019-01-21 22:33:20 +0530

    Basic user Creation complete

--- useraccount/admin.py
+++ useraccount/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import (
+    User,
+)
+from .forms import (
+    UserCreationForm, UserChangeForm
+)
+
+
+
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('full_name','department','mobile','email')
+    list_filter = ('department',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password','mobile','department','sem','college')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('full_name','email', 'password1', 'password2','mobile','department','sem','college')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserAdmin)
+
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
--- useraccount/models.py
+++ useraccount/models.py
@@ -1,3 +1,111 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+    )
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(
+        self,
+        full_name,
+        mobile,
+        email,
+        college,
+        department,
+        sem,
+        ):
+        email = self.normalize_email(email)
+        user = self.model(
+            full_name=full_name,
+            mobile=mobile,
+            email=email,
+            college=college,
+            department=department,
+            sem=sem,
+            )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        full_name,
+        mobile,
+        email,
+        college,
+        department,
+        sem,
+        password):
+        email = self.normalize_email(email)
+        user = self.model(
+            full_name=full_name,
+            mobile=mobile,
+            email=email,
+            college=college,
+            department=department,
+            sem=sem,
+            )
+        user.set_password(password)
+        user.active = True
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    full_name = models.CharField(max_length=255)
+    mobile = models.IntegerField(unique=True)
+    email = models.EmailField(max_length=100,primary_key=True)
+    college = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
+    sem = models.IntegerField()
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    #course_id
+    #score
+
+    class Meta:
+        verbose_name = "Candidate"
+        verbose_name_plural = "Candidates"
+
+    USERNAME_FIELD = 'mobile'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'full_name',
+        'email',
+        'college',
+        'department',
+        'sem',
+    ] 
+
+    def __str__(self):
+        return self.full_name
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        return self.staff
+    @property
+    def is_admin(self):
+        return self.admin
+    def get_full_name(self):
+        return self.full_name
+    def email_user(self, subject, message, from_email=None):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email])
+    objects = UserManager()
