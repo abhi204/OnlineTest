5ccf9f02901a448f7e65a45e4248415dab84d2cd
Abhi <abhi.chaurasia204@gmail.com>
2019-01-22 00:24:03 +0530

    Basic course model complete

--- courseapp/admin.py
+++ courseapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import CourseModel
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CourseModel, CourseAdmin)
--- courseapp/models.py
+++ courseapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class CourseModel(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, verbose_name="Course ID")
+    name = models.CharField(max_length=100, unique=True, verbose_name="Course Name")
+    content = models.CharField(max_length=255, verbose_name="Course Content")
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+
+    def __str__(self):
+        return self.name    
