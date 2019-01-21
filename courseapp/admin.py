a14073ca56833b31652e6e0e7cecf3633949da34
Abhi <abhi.chaurasia204@gmail.com>
2019-01-22 00:44:11 +0530

    Basic question model complete

--- courseapp/admin.py
+++ courseapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import CourseModel
+from .models import CourseModel, QuestionModel
 # Register your models here.
 
 class CourseAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(CourseModel, CourseAdmin)
+admin.site.register(QuestionModel, CourseAdmin)
--- courseapp/models.py
+++ courseapp/models.py
@@ -12,4 +12,17 @@ class CourseModel(models.Model):
         verbose_name_plural = "Courses"
 
     def __str__(self):
-        return self.name    
+        return self.name
+
+class QuestionModel(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, verbose_name="Question ID")
+    course = models.ForeignKey(CourseModel, on_delete=models.CASCADE)
+    opt_a = models.CharField(max_length=255, blank=False, verbose_name="Option A")
+    opt_b = models.CharField(max_length=255, blank=False, verbose_name="Option B")
+    opt_c = models.CharField(max_length=255, blank=False, verbose_name="Option C")
+    opt_d = models.CharField(max_length=255, blank=False, verbose_name="Option D")
+    correct_opt = models.CharField(max_length=255, blank=False, verbose_name="Correct Option")
+
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
