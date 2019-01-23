d2a516d131afbb4888ceec010cc06824dfc59b5b
Abhi <abhi.chaurasia204@gmail.com>
2019-01-24 01:17:59 +0530

    Basic course creation view complete

--- courseapp/views.py
+++ courseapp/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from useraccount.decorators import admin_required
 from django.contrib.auth.decorators import login_required
+from .forms import CourseForm
+from .models import CourseModel
 
 # Create your views here.
 @login_required
 @admin_required
 def course_list_view(request):
-    if request.method == 'GET':
-        return render(request,"courses.html")
+    courses = CourseModel.objects.all()
+    if request.method == 'POST':
+        course_form = CourseForm(request.POST)
+        if course_form.is_valid():
+            course_form.save()
+            return render(request, "courses.html",{"courses": courses})
+        else:
+            print(f"errors : {course_form.errors}")
+            return render(request, "courses.html", {
+                    "errors": course_form.errors,
+                     "courses": courses
+                     })
+    return render(request, "courses.html", {"courses": courses})
+
 
 @login_required
 @admin_required
