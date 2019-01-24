4f19590efcbd17df8e2f24f42b62a4c343ca0f83
Abhi <abhi.chaurasia204@gmail.com>
2019-01-24 20:13:36 +0530

    Initialize TestModel

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('details/<str:course_name>',views.test_detail),
+    path('<str:course_name>', views.test_start),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, Http404
+from django.shortcuts import render, Http404, HttpResponse
 from courseapp.models import CourseModel
+from .models import TestModel
 
 from django.contrib.auth.decorators import login_required
 from useraccount.decorators import admin_required
@@ -7,10 +8,19 @@ from useraccount.decorators import admin_required
 # Create your views here.
 
 @login_required
-@admin_required
 def test_detail(request, course_name):
     try:
         course_instance = CourseModel.objects.get(name=course_name)
     except CourseModel.DoesNotExist:
         raise Http404("There is no course that matches user's query")
     return render(request, 'testapp/test-details.html',{'course': course_instance})
+
+@login_required
+def test_start(request, course_name):
+    instance, created = TestModel.objects.get_or_create(
+        course_id=course_name,
+        candidate_id=request.user.email)
+    if created:
+        instance.save()
+        print("Instance Saved!!!")
+    return HttpResponse("TEST Begins")
