47c2b1e0cf6a97f96cfa10b63d147a75a0c73ce8
Abhi <abhi.chaurasia204@gmail.com>
2019-01-24 21:29:16 +0530

    create endpoint to fetch questions

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('details/<str:course_name>',views.test_detail),
-    path('<str:course_name>', views.test_start),
+    path('<str:course_name>', views.take_test),
+    path('questions/<str:course_name>',views.get_questions)
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, Http404, HttpResponse
 from courseapp.models import CourseModel
 from .models import TestModel
+from courseapp.models import McqQuestionModel, SubQuestionModel
+import random
+from django.http import JsonResponse
 
 from django.contrib.auth.decorators import login_required
 from useraccount.decorators import admin_required
+
+from django.utils import timezone
 
 # Create your views here.
 
@@ -16,11 +21,33 @@ def test_detail(request, course_name):
     return render(request, 'testapp/test-details.html',{'course': course_instance})
 
 @login_required
-def test_start(request, course_name):
-    instance, created = TestModel.objects.get_or_create(
-        course_id=course_name,
-        candidate_id=request.user.email)
-    if created:
-        instance.save()
-        print("Instance Saved!!!")
-    return HttpResponse("TEST Begins")
+def take_test(request, course_name):
+    if request.method == "GET":
+        instance, created = TestModel.objects.get_or_create(
+            course_id=course_name,
+            candidate_id=request.user.email)
+        if created:
+            instance.save()
+        else:
+            instance.test_start_time = timezone.now()
+            instance.score = 0
+            instance.save()
+        return render(request, "testapp/test.html", {"course": course_name})
+
+@login_required
+def get_questions(request,course_name):
+    if request.method == "GET":
+        sub_questions_all = list(SubQuestionModel.objects.values('question', 'q_id'))
+        mcq_questions_all = list(McqQuestionModel.objects.values(
+            'question',
+            'q_id',
+            'opt_a',
+            'opt_b',
+            'opt_c',
+            'opt_d',
+        ))
+        all_questions = mcq_questions_all
+        all_questions.extend(sub_questions_all)
+        questions_random = random.sample(all_questions,30 if len(all_questions)>30 else len(all_questions))
+
+        return JsonResponse(questions_random, safe=False)
